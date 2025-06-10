e6de470978c56152a9e112e75d579092b405e22e
Yunus <nasyus3@gmail.com>
2025-06-10 16:59:07 +0000

    session 3 added

---
+++ session3/classwork/session.py
@@ -0,0 +1,9 @@
+#lst_fruits = ['pineapple', 'pear', 'kiwi', 'apricot', 'peach', 'pear', 'cherry', 'melon']
+
+#inp_char = input('Type a character: ')
+
+#for fruit in lst_fruits:
+#    if inp_char in fruit:
+ #       print(fruit)
+
+the 
