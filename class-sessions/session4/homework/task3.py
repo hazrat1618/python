8f80288590e5d84d10642ff8730584780b1c6548
keepcodingdaily <ynasriddinov@akumoproject.io>
2026-05-08 20:56:05 -0400

    update hometask ot consider inp as int

--- class-sessions/session4/homework/task3.py
+++ class-sessions/session4/homework/task3.py
@@ -1,8 +1,10 @@
-inp = input('Type a list of numbers: ').split()
+inp = input("type a list of number: ").split()
 
 new_list = []
+
 for char in inp:
-    if char not in new_list:
-        new_list.append(char)
+    num = int(char)
+    if num not in new_list:
+        new_list.append(num)
 
 print(new_list)
